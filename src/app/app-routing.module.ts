afc3162f38fb206c5c652aef0ef2d1adbb069588
acejarvis <acejarvis98@gmail.com>
2019-09-19 12:52:06 -0400

    added map & cluster component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MapComponent } from './map/map.component';
+import { ClusterChartComponent } from './cluster-chart/cluster-chart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'cluster', component: ClusterChartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
